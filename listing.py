4b3c03278220a50be0f9348f3c13c4e6fc70943f
mutue002 <118204353+mutue002@users.noreply.github.com>
2025-03-09 14:09:42 +0000

    finish

---
+++ listing.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Rechenoperation mit Py
+
+#Var
+a = 10
+b = 5
+c = 3
+
+print('Sum:', a + b)
+print('Differenz:', a - b)
+print('Produkt:', a * b)
+print('Quotient:', a / b)
+print('Ganzzahlige Quotient:', a // c)
+print('Modulo:', a % c)
+print('Potenzierung (2 hoch 3):', 2 ** c)
